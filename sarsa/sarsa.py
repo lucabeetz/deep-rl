e61f217c7c34a486a12775a99e05cfe04335ee3f
Luca Beetz <luca-beetz@tutanota.com>
2021-04-11 22:27:17 +0200

    Add docstrings

--- sarsa/sarsa.py
+++ sarsa/sarsa.py
@@ -1,30 +1,49 @@
-import gym
-import torch
+"""
+This is an implementation of the SARSA algorithm using a simple neural network as
+function approximator
+"""
+
 import math
 import random
+import gym
+import torch
 import numpy as np
 from torch import nn
 import matplotlib.pyplot as plt
 
 
 class Memory():
+    """
+    Memory class for storing on-policy experiences for training
+    """
+
     def __init__(self):
         self.data_keys = ['states', 'actions', 'rewards', 'next_states', 'dones']
         self.size = 0
         self.reset()
 
     def reset(self):
+        """
+        Delete all experiences stored in memory
+        """
         for k in self.data_keys:
             setattr(self, k, [])
         self.size = 0
 
     def add_experience(self, state, action, reward, next_state, done):
+        """
+        Add a new experience to memory
+        """
         experience = (state, action, reward, next_state, done)
         for i, k in enumerate(self.data_keys):
             getattr(self, k).append(experience[i])
         self.size += 1
 
     def sample(self):
+        """
+        Return all experiences currently stored in memory and create an additional list containing
+        all next_actions needed for calculating the Sarsa target
+        """
         batch = {k: getattr(self, k) for k in self.data_keys}
         batch['next_actions'] = np.zeros_like(batch['actions'])
         batch['next_actions'][:-1] = batch['actions'][1:]
@@ -33,11 +52,16 @@ class Memory():
         for k in batch:
             batch[k] = np.array(batch[k])
             batch[k] = torch.from_numpy(batch[k].astype(np.float32))
+
         self.reset()
         return batch
 
 
 class QNet(nn.Module):
+    """
+    NN used as function approximator for the Q-function
+    """
+
     def __init__(self, input_dim, output_dim):
         super(QNet, self).__init__()
         self.model = nn.Sequential(
@@ -51,6 +75,10 @@ class QNet(nn.Module):
 
 
 class Sarsa():
+    """
+    Class for a Sarsa agent
+    """
+
     def __init__(self, qnet, n_actions, gamma, eps_start, eps_end, eps_decay):
         self.qnet = qnet
         self.n_actions = n_actions
@@ -61,6 +89,9 @@ class Sarsa():
         self.eps_decay = eps_decay
 
     def act(self, state, timestep=1e5):
+        """
+        Pick an action according to an epsilon-greedy policy with a linearly decaying epsilon
+        """
         sample = random.random()
         eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
             math.exp(-1. * timestep / self.eps_decay)
@@ -75,6 +106,10 @@ class Sarsa():
             return torch.tensor(random.randrange(self.n_actions), dtype=torch.long)
 
     def calc_q_loss(self, batch):
+        """
+        Calculate the SARSA Q-loss
+        loss = (Q(a, s) - r + gamma * Q(a_next, s_next))**2
+        """
         states = batch['states']
         next_states = batch['next_states']
 
@@ -91,6 +126,9 @@ class Sarsa():
         return q_loss
 
     def train(self, batch, optim):
+        """
+        Run one optimization step
+        """
         loss = self.calc_q_loss(batch)
         optim.zero_grad()
         loss.backward()
@@ -99,6 +137,9 @@ class Sarsa():
 
 
 def evaluate(agent):
+    """
+    Evaluate the current agent on a single episode
+    """
     env = gym.make('CartPole-v0')
     state = env.reset()
     done = False
@@ -111,6 +152,9 @@ def evaluate(agent):
 
 
 def run_trial():
+    """
+    Run a single trial of training
+    """
     env = gym.make('CartPole-v0')
     obs_dim = env.observation_space.shape[0]
     n_actions = env.action_space.n
@@ -127,16 +171,20 @@ def run_trial():
         state = env.reset()
         done = False
         while not done:
+            # Pick action and run a single environment step
             action = agent.act(state, timestep).item()
             next_state, reward, done, _ = env.step(action)
+            # Add experience to memory for training
             memory.add_experience(state, action, reward, next_state, done)
 
             state = next_state
 
+            # Run a single training step every 32 timesteps
             if timestep % 32 == 0:
                 batch = memory.sample()
-                loss = agent.train(batch, optim)
+                agent.train(batch, optim)
 
+            # Evaluate the current agent every 1000 agents
             if timestep % 1000 == 0:
                 eval_return = evaluate(agent)
                 return_hist.append(eval_return)
@@ -147,6 +195,9 @@ def run_trial():
 
 
 def main():
+    """
+    Runs multiple trials of the implemented SARSA agent
+    """
     all_returns = []
 
     for i in range(10):
