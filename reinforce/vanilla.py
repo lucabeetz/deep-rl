6e3377fc1284a19280aa9943edbc66bcb3123c54
Luca Beetz <luca-beetz@tutanota.com>
2021-04-24 16:45:58 +0200

    Add docstrings to REINFORCE agent

--- reinforce/vanilla.py
+++ reinforce/vanilla.py
@@ -7,10 +7,11 @@ from torch.distributions.categorical import Categorical
 
 gamma = 0.99
 alpha = 1e-2
-num_of_episodes = 300
 
 
 class Policy(nn.Module):
+    """Policy network for taking actions, storing rewards and action probabilites"""
+
     def __init__(self, obs_dims, n_actions):
         super().__init__()
         self.model = nn.Sequential(
@@ -24,10 +25,12 @@ class Policy(nn.Module):
         return self.model(x)
 
     def reset(self):
+        """Delete stored rewards and action log probs"""
         self.rewards = []
         self.log_probs = []
 
     def act(self, obs, eval=False):
+        """Pick action using a softmax distribution"""
         x = torch.tensor(obs, dtype=torch.float32)
         pdparams = self.forward(x)
         pd = Categorical(logits=pdparams)
@@ -38,9 +41,11 @@ class Policy(nn.Module):
 
 
 def train(policy, optimizer):
+    """Run a single training step on the policy network"""
     T = len(policy.rewards)
     returns = torch.empty(T)
     total_return = 0
+    # Calculate "reward to go" for each timestep
     for i in reversed(range(T)):
         total_return = policy.rewards[i] + gamma * total_return
         returns[i] = total_return
@@ -54,19 +59,21 @@ def train(policy, optimizer):
     optimizer.step()
 
 
-def evaluate(agent):
+def evaluate_policy(policy):
+    """Evaluate policy by running a single episode"""
     env = gym.make('CartPole-v0')
     state = env.reset()
     done = False
     episode_return = 0
     while not done:
-        action = agent.act(state, eval=True).item()
+        action = policy.act(state, eval=True).item()
         state, reward, done, _ = env.step(action)
         episode_return += reward
     return episode_return
 
 
 def run_trial():
+    """Run single trial for 10000 training steps"""
     env = gym.make('CartPole-v0')
     obs_dim = env.observation_space.shape[0]
     n_actions = env.action_space.n
@@ -90,6 +97,7 @@ def run_trial():
 
             timestep += 1
 
+        # Train policy after each episode
         train(policy, optimizer)
         policy.reset()
 
@@ -97,6 +105,7 @@ def run_trial():
 
 
 def main():
+    """Run a REINFORCE agent for ten trials on the CartPole-v0 env and plot results"""
     all_returns = []
 
     for i in range(10):
@@ -109,7 +118,7 @@ def main():
 
     x = range(mean_returns.shape[0])
     plt.plot(x, mean_returns)
-    plt.title('Mean return over 10 trials')
+    plt.title('Mean return over 10 trials - REINFORCE')
     plt.fill_between(x, mean_returns - std_returns, mean_returns + std_returns, alpha=0.2)
     plt.ylabel('Mean return')
     plt.xlabel('1000 frames')
